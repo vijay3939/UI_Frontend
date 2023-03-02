056dff522006a0448d33c10d713b40b79c8c7e76
vijay <pvijay3939@gmail.com>
2023-03-02 17:45:49 +0530

    Javascript added

---
+++ javascript/droup-down/practies1.js
@@ -0,0 +1,71 @@
+var Indianstates = [
+    {
+        state: "andra",
+        districts: [
+            "chittoor",
+            "kadapa",
+            "vishakapattanam"
+        ],
+    },
+    {
+        state: "tamil",
+        districts: [
+            "vellore",
+            "chennai",
+            "kanya kumari",
+        ],
+    }, {
+        state: "karnataka",
+        districts: [
+            "bangalore",
+            "mangalore",
+            "bllari"
+        ],
+    },
+    {
+        state: "andra",
+        districts: [
+            "chittoor",
+            "kadapa",
+            "vishakapattanam"
+        ],
+    },
+    {
+        state: "tamil",
+        districts: [
+            "vellore",
+            "chennai",
+            "kanya kumari",
+        ],
+    }, {
+        state: "karnataka",
+        districts: [
+            "bangalore",
+            "mangalore",
+            "bllari"
+        ],
+    },
+    {
+        state: "andra",
+        districts: [
+            "chittoor",
+            "kadapa",
+            "vishakapattanam"
+        ],
+    },
+    {
+        state: "tamil",
+        districts: [
+            "vellore",
+            "chennai",
+            "kanya kumari",
+        ],
+    }, {
+        state: "karnataka",
+        districts: [
+            "bangalore",
+            "mangalore",
+            "bllari"
+        ],
+    },
+];
---
+++ javascript/droup-down/practies2.js
@@ -0,0 +1,42 @@
+var Indianstates = [
+    {
+        state: "Andhra pradesh",
+        districts: [
+            "chittoor",
+            "kadapa",
+            "guntur",
+            "prakasm",
+            "vishakapattanam",
+        ],
+    },
+    {
+        state: "Tamil Nadu",
+        districts: [
+            "Vellore",
+            "chennai",
+            "Kanya kumari",
+            "Madhurai",
+            "Thirruchi",
+        ],
+    },
+    {
+        state: "Karnataka",
+        districts: [
+            "Bangalore",
+            "Bllari",
+            "Mangalore",
+            "prakasm",
+            "vishakapattanam",
+        ],
+    },
+    {
+        state: "Karala",
+        districts: [
+            "Kochi",
+            "Thiruvanamdhapuram",
+            "Pallakadu",
+            "metupalam",
+            "kotagiri",
+        ],
+    },
+]
