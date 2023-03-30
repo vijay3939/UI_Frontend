020357ec3c25ecf9f910a6da0dd4e7c714766f80
vijay <pvijay3939@gmail.com>
2023-03-30 11:21:01 +0530

    sample

---
+++ javascript/selected option/select2.js
@@ -0,0 +1,73 @@
+
+
+var NAME = [
+    {
+        state: "andhra pradesh",
+        district: [
+            "chittoor",
+            "tirupathi",
+            "kadapa",
+            "anthapuram",
+            "karnul",
+            "vijaywada"
+        ],
+    },
+
+    {
+        state: "tamilnadu",
+        district: [
+            "vellore",
+            "ranipet",
+            "chennai",
+            "madhurai",
+            "thiruchi",
+            "kanyakumari",
+            "coimbatore"
+        ],
+    },
+    {
+        state: "kerala",
+        district: [
+            "kottayam",
+            "thiruvanathapuram",
+            "wayanad",
+            "kannur",
+            "palakked",
+            "thrissur",
+            "kozhikode",
+            "kollam",
+            "idukki",
+            "ernakulam"
+        ],
+    },
+    {
+        state: "telangana",
+        district: [
+            "adilabad",
+            "karimnagar",
+            "rengareddy",
+            "warangal",
+            "mahabubnagar",
+            "nalgonda",
+            "medak",
+            "khammam",
+            "mahabubabad"
+        ],
+    },
+
+    {
+        state: "karnataka",
+        district: [
+            "bengalore",
+            "belagavi",
+            "vijaypure",
+            "udupi",
+            "ballari",
+            "kodagu",
+            "kodagu",
+            "dharwad"
+        ],
+    },
+
+
+]
